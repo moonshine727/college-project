bb4639ad9523cbece3ef8ae439734a5c6d1c733b
moonshine727 <145051417+moonshine727@users.noreply.github.com>
2023-09-14 21:14:05 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+string date = DateTime.Now.ToString("dddd, dd.MM.yyyy");
+
+
+Console.WriteLine("Добрый день, Даниил и Виктория!");
+Console.WriteLine($"Сегодня {date}");
+
+Console.ReadLine();
